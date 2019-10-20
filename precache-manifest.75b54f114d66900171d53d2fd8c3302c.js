fc836b6eb59ee5355bc2cbdf58cd8c91c6852101
madhusujan <pmadhusujan@gmail.com>
2019-10-20 19:31:48 -0400

    Updates

---
+++ precache-manifest.75b54f114d66900171d53d2fd8c3302c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e21d29bb4e59fb057c7566ed84d8782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a49389e9757d5efdff0",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "583cb547a166637a4f87",
+    "url": "/static/js/2.1d6acbec.chunk.js"
+  },
+  {
+    "revision": "1a49389e9757d5efdff0",
+    "url": "/static/js/main.c68ae487.chunk.js"
+  },
+  {
+    "revision": "e032b2cee9ea17bfb7b1",
+    "url": "/static/js/runtime-main.ce0369f2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
